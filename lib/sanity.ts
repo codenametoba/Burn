b054108ef6cb907f98bc6e997ea463eaf92b997d
codenametoba <tobadesola@gmail.com>
2026-09-07 01:48:44 +0100

    Use Sanity server read token

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,14 +1,16 @@
 import { createClient } from "next-sanity";
 import { createImageUrlBuilder } from "@sanity/image-url";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "demo";
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+export const projectId = process.env.SANITY_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "demo";
+export const dataset = process.env.SANITY_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const apiVersion = "2026-09-02";
+const serverReadToken = process.env.SANITY_API_READ_TOKEN || process.env.SANITY_API_TOKEN;
 
 export const sanityClient = createClient({
   projectId,
   dataset,
   apiVersion,
+  token: serverReadToken,
   useCdn: false,
   perspective: "published"
 });
